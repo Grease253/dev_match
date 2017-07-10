86b2ec082c5b17e01a07fbddf056708b2e500608
edwardo1010 <edward.woolery@gmail.com>
2017-07-10 08:23:26 +0000

    contacts controller

--- db/migrate/20170710081000_creation_contacts.rb
+++ db/migrate/20170710081000_creation_contacts.rb
@@ -1,4 +1,10 @@
 class CreationContacts < ActiveRecord::Migration
   def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.text :comments
+      t.timestamps
+    end
   end
 end
