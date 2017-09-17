dc0bfa04f327c4427eacab140e7b071f5ef1b9b4
edwardo1010 <edward.woolery@gmail.com>
2017-09-17 23:14:33 +0000

    Added profiles new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  
+  resources :users do
+    resource :profile
+  end
+  
   resources :contacts
   get '/new_contact_path' => 'contacts#new'
   get '/signup_path' => 'pages#signup'
